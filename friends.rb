12d6d9e71ba88e40b6986da48b6401ca9ca025f0
Harrison Ryan <hwryan12@gmail.com>
2022-11-02 16:27:21 -0500

    Add places to visit

--- friends.rb
+++ friends.rb
@@ -3,3 +3,8 @@ arrays = ["Jack", "Drew", "Bob", "David", "Will"]
 puts arrays.at(2)
 puts arrays.at(4)
 puts arrays.at(0)
+
+arrays = ["Rome", "Paris", "Milan", "Texas", "Brazil"]
+
+puts arrays.pop
+puts arrays.push("Prague")
